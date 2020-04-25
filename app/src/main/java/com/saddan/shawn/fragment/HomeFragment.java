213332fbcc780d45d72590b33c64db75d8af8806
ShawnMuktadir <shawn.muktadir03@gmail.com>
2020-04-26 00:06:14 +0600

    code reformatting

--- app/src/main/java/com/saddan/shawn/fragment/HomeFragment.java
+++ app/src/main/java/com/saddan/shawn/fragment/HomeFragment.java
@@ -29,20 +29,18 @@ import java.util.concurrent.TimeUnit;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment
-{
+public class HomeFragment extends Fragment {
     private Button setbtn;
     private FragmentChangeListener listener;
 
-    private  final String TAG =getClass().getSimpleName() ;
-    private long arrived,departure;
-    private TextView arrivedtimeTV,departuretimeTV,timeDifferenceTV,countDownTV;
+    private final String TAG = getClass().getSimpleName();
+    private long arrived, departure;
+    private TextView arrivedtimeTV, departuretimeTV, timeDifferenceTV, countDownTV;
     private long difference;
     private Button moreBtn;
     private LinearLayout bookedLayout;
     //private boolean fromScheduleFragment=false;
     private Button departureBtn;
-
 
 
     public HomeFragment() {
@@ -54,43 +52,38 @@ public class HomeFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-         View view=inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
 
-        arrivedtimeTV=view.findViewById(R.id.arrivedtimeTV);
-        departuretimeTV=view.findViewById(R.id.departureTimeTV);
-        timeDifferenceTV=view.findViewById(R.id.timeDifferenceTV);
-        countDownTV=view.findViewById(R.id.countDownTV);
-        moreBtn=view.findViewById(R.id.moreBtn);
-        bookedLayout=view.findViewById(R.id.bookedLayout);
-        departureBtn=view.findViewById(R.id.departureBtn);
+        arrivedtimeTV = view.findViewById(R.id.arrivedtimeTV);
+        departuretimeTV = view.findViewById(R.id.departureTimeTV);
+        timeDifferenceTV = view.findViewById(R.id.timeDifferenceTV);
+        countDownTV = view.findViewById(R.id.countDownTV);
+        moreBtn = view.findViewById(R.id.moreBtn);
+        bookedLayout = view.findViewById(R.id.bookedLayout);
+        departureBtn = view.findViewById(R.id.departureBtn);
 
         assert getArguments() != null;
-        if(getArguments().getBoolean("s"))
-        {
+        if (getArguments().getBoolean("s")) {
             bookedLayout.setVisibility(View.VISIBLE);
-            arrived=getArguments().getLong("arrived",0);
-            departure=getArguments().getLong("departure",0);
-            difference=departure-arrived;
+            arrived = getArguments().getLong("arrived", 0);
+            departure = getArguments().getLong("departure", 0);
+            difference = departure - arrived;
 
             setTimer(difference);
 
-            Log.d(TAG, "onCreateView: "+arrived+"    "+departure);
-            Log.d(TAG, "onCreateView: difference:"+difference);
+            Log.d(TAG, "onCreateView: " + arrived + "    " + departure);
+            Log.d(TAG, "onCreateView: difference:" + difference);
 
         }
 
+        setbtn = view.findViewById(R.id.setBtn);
+        listener = (FragmentChangeListener) getActivity();
 
-         setbtn=view.findViewById(R.id.setBtn);
-         listener= (FragmentChangeListener) getActivity();
-
-
-
-         return view;
+        return view;
     }
 
-    private void setTimer(long difference)
-    {
+    private void setTimer(long difference) {
         new CountDownTimer(difference, 1000) {
 
             public void onTick(long millisUntilFinished) {
@@ -105,74 +98,65 @@ public class HomeFragment extends Fragment
 
     @SuppressLint("SetTextI18n")
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
-    {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         setbtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                Bundle bundle=new Bundle();
-                bundle.putBoolean("m",false);
-                ScheduleFragment scheduleFragment=new ScheduleFragment();
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putBoolean("m", false);
+                ScheduleFragment scheduleFragment = new ScheduleFragment();
                 scheduleFragment.setArguments(bundle);
                 listener.FragmentChange(scheduleFragment);
             }
         });
 
-        arrivedtimeTV.setText("Arrived "+getDate(arrived));
-        departuretimeTV.setText("Departure "+getDate(departure));
-        timeDifferenceTV.setText(getTimeDiffrence(difference)+" min");
+        arrivedtimeTV.setText("Arrived " + getDate(arrived));
+        departuretimeTV.setText("Departure " + getDate(departure));
+        timeDifferenceTV.setText(getTimeDiffrence(difference) + " min");
 
         moreBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                ScheduleFragment scheduleFragment=new ScheduleFragment();
-                Bundle bundle=new Bundle();
-                bundle.putBoolean("m",true);
-                bundle.putLong("a",arrived);
-                bundle.putLong("d",departure);
+            public void onClick(View v) {
+                ScheduleFragment scheduleFragment = new ScheduleFragment();
+                Bundle bundle = new Bundle();
+                bundle.putBoolean("m", true);
+                bundle.putLong("a", arrived);
+                bundle.putLong("d", departure);
                 scheduleFragment.setArguments(bundle);
                 listener.FragmentChange(scheduleFragment);
             }
         });
 
-
         departureBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                Dialog dialog=new Dialog(requireActivity());
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(requireActivity());
                 dialog.setContentView(R.layout.voucher_dialog);
-                DialogHelper dialogHelper=new DialogHelper(dialog,requireActivity(),getDate(arrived),getDate(departure),getTimeDiffrence(difference),difference);
+                DialogHelper dialogHelper = new DialogHelper(dialog, requireActivity(), getDate(arrived), getDate(departure), getTimeDiffrence(difference), difference);
                 dialogHelper.initDialog();
                 dialog.show();
             }
         });
-
     }
 
     @SuppressLint("DefaultLocale")
-    private String getTimeDiffrence(long difference){
+    private String getTimeDiffrence(long difference) {
 
-        return   String.format("%02d:%02d",
+        return String.format("%02d:%02d",
                 TimeUnit.MILLISECONDS.toHours(difference),
                 TimeUnit.MILLISECONDS.toMinutes(difference) -
                         TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(difference)) // The change is in this line
         );
     }
 
-    private String getDate(long milliSeconds)
-    {
+    private String getDate(long milliSeconds) {
         // Create a DateFormatter object for displaying date in specified format.
-
         SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yyyy, hh:mm a", Locale.US);
         // Create a calendar object that will convert the date and time value in milliseconds to date.
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(milliSeconds);
         return formatter.format(calendar.getTime());
     }
-
 }
--- app/src/main/java/com/saddan/shawn/fragment/ScheduleFragment.java
+++ app/src/main/java/com/saddan/shawn/fragment/ScheduleFragment.java
@@ -21,114 +21,102 @@ import com.saddan.shawn.listener.FragmentChangeListener;
 import java.util.Date;
 
 
-public class ScheduleFragment extends Fragment
-{
-    private  final String TAG =getClass().getSimpleName();
-    private Button setBtn,cancelBtn;
-    private SingleDateAndTimePicker arrivedPicker,departurePicker;
-    private Date arrivedDate,departerDate;
-    private LinearLayout arriveDisableLayout,departureDisableLayout;
-    private boolean setArrivedDate=false;
-    private boolean more=false;
+public class ScheduleFragment extends Fragment {
+    private final String TAG = getClass().getSimpleName();
+    private Button setBtn, cancelBtn;
+    private SingleDateAndTimePicker arrivedPicker, departurePicker;
+    private Date arrivedDate, departerDate;
+    private LinearLayout arriveDisableLayout, departureDisableLayout;
+    private boolean setArrivedDate = false;
+    private boolean more = false;
     private FragmentChangeListener listener;
-    public ScheduleFragment()
-    {
+
+    public ScheduleFragment() {
         // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
-    {
+                             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-         View view=inflater.inflate(R.layout.fragment_schedule, container, false);
+        View view = inflater.inflate(R.layout.fragment_schedule, container, false);
 
-         arrivedPicker=view.findViewById(R.id.arrivedPicker);
-         departurePicker=view.findViewById(R.id.departurePicker);
-         setBtn=view.findViewById(R.id.saveBtn);
-         cancelBtn=view.findViewById(R.id.cancelBtn);
-         arriveDisableLayout =view.findViewById(R.id.arrivedLayout);
-         departureDisableLayout=view.findViewById(R.id.departureDisable);
+        arrivedPicker = view.findViewById(R.id.arrivedPicker);
+        departurePicker = view.findViewById(R.id.departurePicker);
+        setBtn = view.findViewById(R.id.saveBtn);
+        cancelBtn = view.findViewById(R.id.cancelBtn);
+        arriveDisableLayout = view.findViewById(R.id.arrivedLayout);
+        departureDisableLayout = view.findViewById(R.id.departureDisable);
 
-         listener= (FragmentChangeListener) getActivity();
+        listener = (FragmentChangeListener) getActivity();
 
-         return view;
+        return view;
     }
 
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
-    {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         Log.d(TAG, "onViewCreated: ");
 
-
         //assert getArguments() != null;
         /*if(getArguments()!=null)
         {*/
-         more=getArguments().getBoolean("m");
+        if (getArguments() != null) {
+            more = getArguments().getBoolean("m");
+        }
         //}
         arrivedPicker.setIsAmPm(true);
         departurePicker.setIsAmPm(true);
 
-        if(more)
-        {
-               setArrivedDate=true;
-               arrivedPicker.setEnabled(false);
-               arriveDisableLayout.setBackgroundColor(getResources().getColor(R.color.disableColor));
-               Date arrived=new Date(getArguments().getLong("a"));
-               Date departure=new Date(getArguments().getLong("d"));
+        if (more) {
+            setArrivedDate = true;
+            arrivedPicker.setEnabled(false);
+            arriveDisableLayout.setBackgroundColor(getResources().getColor(R.color.disableColor));
+            Date arrived = new Date(getArguments().getLong("a"));
+            Date departure = new Date(getArguments().getLong("d"));
 
-               arrivedDate=arrived;
-               departerDate=departure;
+            arrivedDate = arrived;
+            departerDate = departure;
 
-               arrivedPicker.setDefaultDate(arrived);
-               departurePicker.setDefaultDate(departure);
+            arrivedPicker.setDefaultDate(arrived);
+            departurePicker.setDefaultDate(departure);
 
-        }else {
+        } else {
             departurePicker.setEnabled(false);
             departureDisableLayout.setBackgroundColor(getResources().getColor(R.color.disableColor));
 
-            arrivedDate=arrivedPicker.getDate();
-            Log.d(TAG, "arrived date before scrolling "+arrivedDate);
-            departerDate=departurePicker.getDate();
-            Log.d(TAG, "departure date before scrolling "+departerDate);
-
+            arrivedDate = arrivedPicker.getDate();
+            Log.d(TAG, "arrived date before scrolling " + arrivedDate);
+            departerDate = departurePicker.getDate();
+            Log.d(TAG, "departure date before scrolling " + departerDate);
         }
-
-
-
 
         arrivedPicker.addOnDateChangedListener(new SingleDateAndTimePicker.OnDateChangedListener() {
             @Override
-            public void onDateChanged(String displayed, Date date)
-            {
-                arrivedDate=date;
-                Log.d(TAG, "onDateChanged: "+date);
-                Log.d(TAG, "onDateChanged: arrivedDate"+arrivedDate);
+            public void onDateChanged(String displayed, Date date) {
+                arrivedDate = date;
+                Log.d(TAG, "onDateChanged: " + date);
+                Log.d(TAG, "onDateChanged: arrivedDate" + arrivedDate);
             }
         });
 
         departurePicker.addOnDateChangedListener(new SingleDateAndTimePicker.OnDateChangedListener() {
             @Override
-            public void onDateChanged(String displayed, Date date)
-            {
-                departerDate=date;
-                Log.d(TAG, "onDateChanged: "+date);
-                Log.d(TAG, "onDateChanged: departureDate"+departerDate);
+            public void onDateChanged(String displayed, Date date) {
+                departerDate = date;
+                Log.d(TAG, "onDateChanged: " + date);
+                Log.d(TAG, "onDateChanged: departureDate" + departerDate);
             }
         });
 
-        setBtn.setOnClickListener(new View.OnClickListener()
-        {
+        setBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 Log.d(TAG, "onClick: didnot entered to condition");
-                if(!setArrivedDate)
-                {
+                if (!setArrivedDate) {
                     Log.d(TAG, "onClick:  entered to if");
                     arrivedPicker.setEnabled(false);
                     arriveDisableLayout.setBackgroundColor(getResources().getColor(R.color.disableColor));
@@ -136,63 +124,53 @@ public class ScheduleFragment extends Fragment
                     departurePicker.setEnabled(true);
                     departureDisableLayout.setBackgroundColor(getResources().getColor(R.color.enableColor));
 
-                    setArrivedDate=true;
-                }else {
-                      Log.d(TAG, "onClick: didnot entered to else");
-                      if(departerDate.getTime()-arrivedDate.getTime()<0)
-                      {
-                          Toast.makeText(requireActivity(), "Departure time can't less than arrived time", Toast.LENGTH_SHORT).show();
-                      }else {
-                        Bundle bundle=new Bundle();
-                        Log.d(TAG, "onClick: "+arrivedDate.getTime());
-                        Log.d(TAG, "onClick: "+departerDate.getTime());
-                        bundle.putBoolean("s",true);
-                        bundle.putLong("arrived",arrivedDate.getTime());
-                        bundle.putLong("departure",departerDate.getTime());
+                    setArrivedDate = true;
+                } else {
+                    Log.d(TAG, "onClick: didnot entered to else");
+                    if (departerDate.getTime() - arrivedDate.getTime() < 0) {
+                        Toast.makeText(requireActivity(), "Departure time can't less than arrived time", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Bundle bundle = new Bundle();
+                        Log.d(TAG, "onClick: " + arrivedDate.getTime());
+                        Log.d(TAG, "onClick: " + departerDate.getTime());
+                        bundle.putBoolean("s", true);
+                        bundle.putLong("arrived", arrivedDate.getTime());
+                        bundle.putLong("departure", departerDate.getTime());
                         //BookedFragment bookedFragment=new BookedFragment();
                         //bookedFragment.setArguments(bundle);
-                        HomeFragment homeFragment=new HomeFragment();
+                        HomeFragment homeFragment = new HomeFragment();
                         homeFragment.setArguments(bundle);
                         listener.FragmentChange(homeFragment);
                     }
-
                 }
-
-
             }
         });
 
         cancelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                 if(setArrivedDate)
-                 {
-                     arrivedPicker.setEnabled(true);
-                     arriveDisableLayout.setBackgroundColor(getResources().getColor(R.color.enableColor));
-                     setArrivedDate=false;
-                 }else {
+            public void onClick(View v) {
+                if (setArrivedDate) {
+                    arrivedPicker.setEnabled(true);
+                    arriveDisableLayout.setBackgroundColor(getResources().getColor(R.color.enableColor));
+                    setArrivedDate = false;
+                } else {
 
-                 }
+                }
             }
         });
 
     }
 
     @Override
-    public void onStart() 
-    {
-        more=getArguments().getBoolean("m");
-
-        if(more)
-        {
-            setArrivedDate=true;
+    public void onStart() {
+        if (getArguments() != null) {
+            more = getArguments().getBoolean("m");
         }
-        else {
-            setArrivedDate=false;
+        if (more) {
+            setArrivedDate = true;
+        } else {
+            setArrivedDate = false;
         }
-
-
         super.onStart();
         Log.d(TAG, "onStart: ");
     }
